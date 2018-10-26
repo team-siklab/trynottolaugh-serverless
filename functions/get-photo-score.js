49f4523a2caa10905ce1af15257ee2f957580a2c
Richard Neil Ilagan <ilagan@amazon.com>
2018-10-26 16:52:56 +0800

    Adds lambda test scaffold for s3 put event.

--- functions/get-photo-score.js
+++ functions/get-photo-score.js
@@ -1,7 +1,51 @@
+/* -----
+{
+  "Records": [
+    {
+      "eventVersion": "2.0",
+      "eventSource": "aws:s3",
+      "awsRegion": "ap-northeast-1",
+      "eventTime": "1970-01-01T00:00:00.000Z",
+      "eventName": "ObjectCreated:Put",
+      "userIdentity": {
+        "principalId": "EXAMPLE"
+      },
+      "requestParameters": {
+        "sourceIPAddress": "127.0.0.1"
+      },
+      "responseElements": {
+        "x-amz-request-id": "EXAMPLE123456789",
+        "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"
+      },
+      "s3": {
+        "s3SchemaVersion": "1.0",
+        "configurationId": "testConfigRule",
+        "bucket": {
+          "name": "example-bucket",
+          "ownerIdentity": {
+            "principalId": "EXAMPLE"
+          },
+          "arn": "arn:aws:s3:::example-bucket"
+        },
+        "object": {
+          "key": "test/key",
+          "size": 1024,
+          "eTag": "0123456789abcdef0123456789abcdef",
+          "sequencer": "0A1B2C3D4E5F678901"
+        }
+      }
+    }
+  ]
+}
+----- */
+
 const logger = require('../utils/logger.js')
 
 exports.handler = (event, context, callback) => {
-  // logger.debug(':: [get-photo-score] start.')
+  logger.debug(':: [get-photo-score] start.')
+
+  const { s3 } = event.params.Records[0]
+  logger.debug(`:: [get-photo-score] S3 object key is "${s3.object.key}".`)
 
   callback(null, {
     statusCode: 200,
