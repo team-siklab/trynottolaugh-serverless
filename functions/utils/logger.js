5411d3606ad37540137b6a8b2f056f77213d5677
Richard Neil Ilagan <ilagan@amazon.com>
2018-10-28 17:00:50 +0800

    Removes winston-cloudwatch

--- functions/utils/logger.js
+++ functions/utils/logger.js
@@ -1,29 +1,18 @@
 const { createLogger, transports, format } = require('winston')
-// const WinstonCloudWatch = require('winston-cloudwatch')
-
-// const { APPNAME, NODE_ENV } = require('./env')
 
 // :: ---
 
 const consoleTransport = new transports.Console()
-
-// const cloudwatchTransport = new WinstonCloudWatch({
-//   name: 'cloudwatch-logstream',
-//   logGroupName: APPNAME,
-//   logStreamName: NODE_ENV
-// })
 
 const logger = createLogger({
   level: 'debug',
   format: format.simple(),
   transports: [
     consoleTransport
-    // cloudwatchTransport
   ],
   // :: TODO figure something out for this if needed
   exceptionHandlers: [
     consoleTransport
-    // cloudwatchTransport
   ]
 })
 
