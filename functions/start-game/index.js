2d302665427a62c9b9ce04c273df22f54af11b38
Richard Neil Ilagan <ilagan@amazon.com>
2018-10-31 15:31:12 +0800

    Adds scaffolding for start game.

--- functions/start-game/index.js
+++ functions/start-game/index.js
@@ -1,8 +1,39 @@
 require('../utils/aws-init')
 
+const logger = require('../utils/logger.js')
+const { startGame } = require('../utils/ddb')
+
 // :: ---
 
 exports.handler = (event, context, callback) => {
+  logger.start('start-game')
+  logger.debug(JSON.stringify(event))
+
+  const { gameid } = event.pathParameters
+  const { timestamp } = JSON.parse(event.body || {})
+
+  startGame(gameid, timestamp)
+    .then(data => {
+      logger.debug(`:: [start-game] Game started: ${gameid}.`)
+
+      // :: TODO
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(data),
+        headers: {
+          'Access-Control-Allow-Origins': '*',
+          'Access-Control-Allow-Credentials': true
+        }
+      })
+    })
+    .catch(err => {
+      // :: TODO filter based on error type
+      logger.error(`:: [create-game] Error encountered while attempting to start game.`)
+      logger.debug(JSON.stringify(err))
+
+      callback(err)
+    })
+
   // :: TODO
   callback(null, {
     statusCode: 200,
--- functions/utils/ddb.js
+++ functions/utils/ddb.js
@@ -2,7 +2,9 @@ const AWS = require('aws-sdk')
 const { wrap } = require('dynamodb-data-types').AttributeValue
 const uuid = require('uuid/v4')
 const logger = require('./logger')
+
 const { DDB_GAMES, DDB_PHOTOSCORES } = require('./env')
+const { GAME_STATES } = require('./enums')
 
 const ddb = new AWS.DynamoDB()
 
@@ -20,7 +22,7 @@ exports.createGame = (payload) => {
 
   const item = {
     GameId: gameid,
-    Status: 'IN-LOBBY'
+    Status: GAME_STATES.IN_LOBBY
   }
 
   const ddbpayload = {
@@ -38,6 +40,55 @@ exports.createGame = (payload) => {
 
       // :: ---
       logger.debug(`:: Game record generated to DDB: ${gameid}`)
+      logger.info(data)
+
+      return resolve({ gameid })
+    })
+  })
+}
+
+/**
+ * Starts a game.
+ * Will result in an error if the game does not have enough players.
+ *
+ * @param {string} gameid - The Game ID of the game to start.
+ * @param {string} timestamp - Timestamp to mark start of game.
+ */
+exports.startGame = (gameid, timestamp) => {
+  logger.debug(':: Attempting to start game ...')
+
+  // :: TODO check if game exists
+  // :: TODO check for enough players
+  // :: TODO check that game is IN-LOBBY
+
+  const keymap = { GameId: gameid }
+  const updatemap = {
+    ':status': GAME_STATES.ONGOING,
+    ':starttimestamp': timestamp
+  }
+
+  const ddbpayload = {
+    TableName: DDB_GAMES,
+    Key: wrap(keymap),
+    ExpressionAttributeNames: {
+      '#Status': 'Status',
+      '#StartTimestamp': 'StartTimestamp'
+    },
+    ExpressionAttributeValues: wrap(updatemap),
+    UpdateExpression: 'SET #Status = :status, #StartTimestamp = :starttimestamp',
+    ReturnValues: 'ALL_NEW'
+  }
+
+  return new Promise((resolve, reject) => {
+    ddb.updateItem(ddbpayload, (err, data) => {
+      if (err) {
+        logger.error(':: Error encountered while starting game.')
+        logger.debug(JSON.stringify(err))
+        return reject(err)
+      }
+
+      // :: ---
+      logger.debug(`:: Game started: ${gameid}`)
       logger.info(data)
 
       return resolve({ gameid })
