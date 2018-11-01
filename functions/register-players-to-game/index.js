2f8f05bb7feae9dbb456ee937063c8d4340d3ceb
Richard Neil Ilagan <ilagan@amazon.com>
2018-11-01 17:10:36 +0800

    Adds scaffolding for register players.

--- functions/register-players-to-game/index.js
+++ functions/register-players-to-game/index.js
@@ -1,15 +1,27 @@
 require('../utils/aws-init')
 
+const logger = require('../utils/logger.js')
+const { assert } = require('../utils/assert')
+const { CORS_HEADERS } = require('../utils/enums')
+
 // :: ---
 
 exports.handler = (event, context, callback) => {
+  logger.start('register-player')
+  logger.debug(JSON.stringify(event))
+
+  const { gameid } = event.pathParameters
+
+  // :: failsafes
+  const isSafe =
+    assert(gameid, 'No Game Id specified.', callback)
+
+  if (!isSafe) return
+
   // :: TODO
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({}),
-    headers: {
-      // :: TODO determine if this is actually required
-      'Access-Control-Allow-Origins': '*'
-    }
+    body: JSON.stringify(event),
+    headers: { ...CORS_HEADERS }
   })
 }
